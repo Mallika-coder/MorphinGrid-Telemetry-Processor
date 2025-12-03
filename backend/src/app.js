41929de8833f1173194300f749ab76b6a7a9e27b
Mallika Verma <vermamallika372@gmail.com>
2025-12-04 00:49:22 +0530

    new

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const ingestRoutes = require('./routes/ingest.routes');
+const dataRoutes = require('./routes/data.routes');
+require('./config/db')();
+
+const app = express();
+app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(express.json());
+
+app.use('/api/ingest', ingestRoutes);
+app.use('/api/data', dataRoutes);
+
+app.get('/health', (req, res) => res.json({ ok: true }));
+
+module.exports = app;
